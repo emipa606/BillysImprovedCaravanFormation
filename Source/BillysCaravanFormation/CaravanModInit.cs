30e44f6dcf05c84beef91659c5f5e48148858672
Mlie <emipa606@users.noreply.github.com>
2022-01-05 20:39:09 +0100

    Updated by popular demand

--- Source/BillysCaravanFormation/CaravanModInit.cs
+++ Source/BillysCaravanFormation/CaravanModInit.cs
@@ -1,52 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using RimWorld;
-using Verse;
+﻿using System.Reflection;
 using HarmonyLib;
-using System.Reflection;
 using UnityEngine;
+using Verse;
 
-namespace BillysCaravanFormation
+namespace BillysCaravanFormation;
+
+public class CaravanModSettings : ModSettings
 {
-    public class CaravanModSettings : ModSettings
-    {
-        public bool fastAnimalCollection = false;
+    public bool fastAnimalCollection;
 
-        public override void ExposeData()
-        {
-            Scribe_Values.Look(ref fastAnimalCollection, "fastCaravanAnimalCollection");
-            base.ExposeData();
-        }
+    public override void ExposeData()
+    {
+        Scribe_Values.Look(ref fastAnimalCollection, "fastCaravanAnimalCollection");
+        base.ExposeData();
     }
-    public class CaravanModInit : Verse.Mod
+}
+
+public class CaravanModInit : Mod
+{
+    private readonly CaravanModSettings settings;
+
+    public CaravanModInit(ModContentPack content)
+        : base(content)
     {
-        CaravanModSettings settings;
+        settings = GetSettings<CaravanModSettings>();
+        // this should be executed when Rimworld loads our mod (because the class inherits from Verse.Mod). 
+        // The code below tells Harmony to apply the patches attributed above.
+        var harmony = new Harmony("bem.rimworld.mod.billy.smartcaravan");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
 
-        public CaravanModInit(ModContentPack content)
-            : base(content)
-        {
-            this.settings = GetSettings<CaravanModSettings>();
-            // this should be executed when Rimworld loads our mod (because the class inherits from Verse.Mod). 
-            // The code below tells Harmony to apply the patches attributed above.
-            var harmony = new Harmony("bem.rimworld.mod.billy.smartcaravan");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        var listing = new Listing_Standard();
+        listing.Begin(inRect);
+        listing.CheckboxLabeled("Skip gathering of caravan animals by colonists", ref settings.fastAnimalCollection,
+            "Animals will move to the meeting spot on their own, speeding up caravan formation");
+        listing.End();
+        base.DoSettingsWindowContents(inRect);
+    }
 
-        }
-
-        public override void DoSettingsWindowContents(Rect inRect)
-        {
-            Listing_Standard listing = new Listing_Standard();
-            listing.Begin(inRect);
-            listing.CheckboxLabeled("Skip gathering of caravan animals by colonists", ref settings.fastAnimalCollection, "Animals will move to the meeting spot on their own, speeding up caravan formation");
-            listing.End();
-            base.DoSettingsWindowContents(inRect);
-        }
-
-        public override string SettingsCategory()
-        {
-            return "Billy's Improved Caravan Formation";
-        }
+    public override string SettingsCategory()
+    {
+        return "Billy's Improved Caravan Formation";
     }
 }
